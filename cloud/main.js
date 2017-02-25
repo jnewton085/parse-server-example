6b47029acbdf18da5d7959fb135d0f64fb606c20
Jeremy Newton <jeremynewt@gmail.com>
2017-02-25 12:11:53 -0800

    Added ability to change User passwords

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,15 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define('resetPassword', function(req, res) {
+	var query = new Parse.query("_User");
+	query.get(request.params.userId, {userMasterKey: true }).then((user) {
+		user.setPassword(request.params.newPassword);
+		return user.save(null, {useMaterKey: true});
+	    }).then((user) => {
+		    response.success(JSON.stringify(user))
+		}, (obj, error) => {
+		    response.error(error.message);
+		});
+    });
