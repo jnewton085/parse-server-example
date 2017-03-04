a1163502ebbfd1381c5f1c3962a7b7c86af2dedb
Jeremy Newton <jeremynewt@gmail.com>
2017-03-04 13:38:27 -0800

    new resetPassword

--- cloud/main.js
+++ cloud/main.js
@@ -7,30 +7,17 @@ Parse.Cloud.define('hello2', function(req, res) {
 	res.success('Hi2');
     });
 
-Parse.Cloud.define('testSetPasswordForUser', function(request, response) {
-	var query = new Parse.Query("_User");
-	query.get(request.params.userId, { useMasterKey: true }).then((user) {
-		user.setPassword(request.params.newPassword);
-		return user.save(null, { useMasterKey: true });
-	    }).then((user) => {
-		    response.success(JSON.stringify(user));
-		}, (obj, error) => { 
-		    response.error(error.message);
-		});
-    });
-
-
-//Parse.Cloud.define('resetPassword', function(req, res) {
-//	var query = new Parse.Query('_User');
-//	query.get(req.params.userId, {
-//		useMasterKey: true, 
-//		success: function(user) {
-//		    user.setPassword(req.params.newPassword);
-//		    user.save(null, { useMasterKey: true });
-//		    res.success('Saved User!')
-//		},
-//		error: function(obj, error) {
-//		    res.error('Errored!');
-//		}
-//	    });
-//});
+Parse.Cloud.define('resetPassword', function(req, res) {
+	var query = new Parse.Query('_User');
+	query.get(req.params.userId, {
+		useMasterKey: true, 
+		success: function(user) {
+		    user.setPassword(req.params.newPassword);
+		    user.save(null, { useMasterKey: true });
+		    res.success('Saved User! '+req.params.userId+req.params.newPassword)
+		},
+		error: function(obj, error) {
+		    res.error('Errored!');
+		}
+	    });
+});
