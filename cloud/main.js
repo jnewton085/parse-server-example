e177f5967e7f1e98deca0380a3c483e47ffa692d
Jeremy Newton <jeremynewt@gmail.com>
2017-03-04 11:37:43 -0800

    Added user defined cloud function

--- cloud/main.js
+++ cloud/main.js
@@ -3,14 +3,17 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
 
-Parse.Cloud.define('resetPassword', function(request, response) {
-	var query = new Parse.query("_User");
-	query.get(request.params.userId, {userMasterKey: true }).then((user) {
-		user.setPassword(request.params.newPassword);
-		return user.save(null, {useMaterKey: true});
-	    }).then((user) => {
-		    response.success(JSON.stringify(user))
-		}, (obj, error) => {
-		    response.error(error.message);
-		});
+Parse.Cloud.define('hello2', function(req, res) {
+	res.success('Hi2');
     });
+//Parse.Cloud.define('resetPassword', function(request, response) {
+//	var query = new Parse.query("_User");
+//	query.get(request.params.userId, {userMasterKey: true }).then((user) {
+//		user.setPassword(request.params.newPassword);
+//		return user.save(null, {useMaterKey: true});
+//	}).then((user) => {
+//		    response.success(JSON.stringify(user))
+//		}, (obj, error) => {
+//		    response.error(error.message);
+//	});
+//});
