2f0588409330982bb7d254a94c32bd6ceb0a489a
Jeremy Newton <jeremynewt@gmail.com>
2017-03-04 13:06:32 -0800

    new resetPassword

--- cloud/main.js
+++ cloud/main.js
@@ -9,7 +9,7 @@ Parse.Cloud.define('hello2', function(req, res) {
 
 Parse.Cloud.define('resetPassword', function(req, res) {
 	var query = new Parse.Query('_User');
-	res.success('HI3');
+	res.success(req.params.userId);
 	//query.get(req.params.userId, {userMasterKey: true }).then((user) {
 	//	user.setPassword(req.params.newPassword);
 	//	return user.save(null, {useMasterKey: true});
