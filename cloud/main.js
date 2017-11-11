608dfb7c14008ec247c556020209da3c9ba8eee4
Jeremy Newton <jeremynewt@gmail.com>
2017-11-11 12:08:27 -0500

    Added cloud code for sending test email

--- cloud/main.js
+++ cloud/main.js
@@ -7,6 +7,18 @@ Parse.Cloud.define('hello2', function(req, res) {
 	res.success('Hi2');
     });
 
+Parse.Cloud.define('test_email', function(req, res) {
+	var mailgun = require('mailgun.js');
+	var mg = mailgun.client({username: 'api', key: process.env.MAILGUN_KEY});
+	mg.messages.create(process.env.DOMAIN, { 
+		from: process.env.MAILGUN_FROM_ADDRESS,
+		    to: ["jeremynewt@gmail.com"],
+		    subject: "Hellow",
+		    text: "orld",
+		    html: "<h1> Just testing out ye olde mailgun </h1>"
+		    }).then(msg => console.log(msg)).catch(err => res.error(err));
+	res.success('Yes!');
+    });
 Parse.Cloud.define("changeUserPassword", function(request, response) {
                    // Set up to modify user data
                    Parse.Cloud.useMasterKey();
