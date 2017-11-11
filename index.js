1c0eb275ef2a8a83506b62b3fbd8541a1d445b4b
Jeremy Newton <jeremynewt@gmail.com>
2017-11-11 16:12:41 -0500

    Adding adapter...fixing typo

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ var api = new ParseServer({
   //verifyUserEmails: true,
   publicServerURL: process.env.PUBLIC_SERVER_URL || 'http://localhost:1337/parse',
   appName: process.env.APP_NAME || '<your app name?',
-  //emailAdapter: {module: 'parse-server-simple-mailgun-adapter', options: {fromAddress: process.env.MAILGUN_FROM_ADRESS, domain: process.env.DOMAIN, apiKey: process.env.MAILGUN_KEY},
+  emailAdapter: {module: 'parse-server-simple-mailgun-adapter', options: {fromAddress: process.env.MAILGUN_FROM_ADRESS, domain: process.env.DOMAIN, apiKey: process.env.MAILGUN_KEY}},
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
