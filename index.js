bc7da5de969690d82278f1214772ba88f903a734
Jeremy Newton <jeremynewt@gmail.com>
2017-11-17 00:23:46 -0500

    Added privacy page, and removed test page

--- index.js
+++ index.js
@@ -56,9 +56,14 @@ app.get('/', function(req, res) {
 
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
-app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
-});
+//app.get('/test', function(req, res) {
+//  res.sendFile(path.join(__dirname, '/public/test.html'));
+//});
+
+// The Privacy Policy statement
+app.get('/privacy', function(req, res) {
+	res.sendFile(path.join(__dirname, '/public/privacypolicy.htm'));
+    });
 
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
